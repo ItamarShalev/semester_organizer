a9ae1e710f76b98400f6d2c53381e056f97078eb
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 00:17:56 +0200

    data: course: Fill init function.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/course.py
+++ data/course.py
@@ -1,5 +1,5 @@
 class Course:
-    def __init__(self):
-        self.name = None
-        self.course_number = None
-        self.parent_course_number = None
+    def __init__(self, name: str, course_number: str, parent_course_number: str):
+        self.name = name
+        self.course_number = course_number
+        self.parent_course_number = parent_course_number
