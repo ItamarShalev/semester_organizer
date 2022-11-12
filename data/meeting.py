1aa2dfdb45e1bcd481dbab92a57718a95697646e
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 00:16:08 +0200

    data: meeting: Fill init function.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/meeting.py
+++ data/meeting.py
@@ -1,6 +1,10 @@
+from time import struct_time
+from day import Day
+
+
 class Meeting:
 
-    def __init__(self):
-        self.day = None
-        self.start_time = None
-        self.end_time = None
+    def __init__(self, day: Day, start_time: struct_time, end_time: struct_time):
+        self.day = day
+        self.start_time = start_time
+        self.end_time = end_time
