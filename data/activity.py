5a4aee58143369b00dfff4eb7c1679ba199fad4c
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 00:57:52 +0200

    activity: Fill init function and add_slot function.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/activity.py
+++ data/activity.py
@@ -1,10 +1,23 @@
+from data.type import Type
+from data.day import Day
+
+
 class Activity:
 
-    def __init__(self):
-        self.name = None
-        self.type = None
-        self.is_must = False
-        self.days = [[]] * 7
+    def __init__(self, name: str, activity_type: Type, is_must: bool):
+        self.name = name
+        self.type = activity_type
+        self.is_must = is_must
+        self.days = {day: [] for day in Day}
 
     def add_slot(self, meeting):
-        self.days[meeting.day.value] += meeting
+        if meeting.is_crash_with_meeting(self.days[meeting.day]):
+            raise Exception("Meeting is crash with other meeting")
+        self.days[meeting.day].append(meeting)
+
+    def is_free_slot(self, meeting):
+        return not meeting.is_crash_with_meeting(self.days[meeting.day])
+
+    def add_slots(self, meetings):
+        for meeting in meetings:
+            self.add_slot(meeting)
