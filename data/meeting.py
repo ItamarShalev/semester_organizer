b7486303291ad141177721013137cf8f64f3a478
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 00:43:49 +0200

    data: meeting: Add functions to check if meeting crash another.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/meeting.py
+++ data/meeting.py
@@ -1,4 +1,5 @@
-from time import struct_time
+from time import struct_time, time
+
 from day import Day
 
 
@@ -8,3 +9,20 @@ class Meeting:
         self.day = day
         self.start_time = start_time
         self.end_time = end_time
+
+    def __str__(self):
+        start_time_str = time.strftime("%H:%M", self.start_time)
+        end_time_str = time.strftime("%H:%M", self.end_time)
+        return f"{start_time_str} - {end_time_str}"
+
+    def is_crash_with_meeting(self, meeting):
+        if self.day != meeting.day:
+            return False
+        meeting_crash = self.start_time <= meeting.start_time < self.end_time
+        other_meeting_crash = meeting.start_time <= self.start_time < meeting.end_time
+        return meeting_crash or other_meeting_crash
+
+    def is_crash_with_meetings(self, meetings):
+        if not meetings:
+            return False
+        return any(self.is_crash_with_meeting(meeting) for meeting in meetings)
