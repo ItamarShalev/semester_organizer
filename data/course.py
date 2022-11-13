0514f8ebc113bc4347ea2f98adfb11cfc65e02d0
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 04:50:37 +0200

    data: course: Fix course_number and parent_course_number type to int.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/course.py
+++ data/course.py
@@ -1,5 +1,8 @@
+
+
 class Course:
-    def __init__(self, name: str, course_number: str, parent_course_number: str):
+
+    def __init__(self, name: str, course_number: int, parent_course_number: int):
         self.name = name
         self.course_number = course_number
         self.parent_course_number = parent_course_number
