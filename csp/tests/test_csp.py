5f976c1a21eca60c6a252e270521a1f92a5bd3d6
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 22:04:23 +0200

    tests: csp: Add tests for csp module.
    test_two_from_two_options
    test_one_from_two_options
    test_no_option
    
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- csp/tests/test_csp.py
+++ csp/tests/test_csp.py
@@ -27,3 +27,76 @@ def test_one_from_one_option():
     schedules = csp.extract_schedules(activities)
     assert len(schedules) == 1
     assert any(schedule.contains(activities) for schedule in schedules)
+
+
+def test_two_from_two_options():
+    activities_option_1 = []
+    activities_option_2 = []
+
+    activities = []
+    academic_activity = AcademicActivity("a", Type.LECTURE, True, "a", 1, 1, "a")
+    academic_activity.add_slot(Meeting(Day.SUNDAY, _format_str("10:00"), _format_str("11:00")))
+    activities.append(academic_activity)
+    activities_option_1.append(academic_activity)
+
+    academic_activity = AcademicActivity("a", Type.LECTURE, True, "a", 2, 2, "a")
+    academic_activity.add_slot(Meeting(Day.SUNDAY, _format_str("12:00"), _format_str("14:30")))
+    activities.append(academic_activity)
+    activities_option_2.append(academic_activity)
+
+    activity = Activity("c", Type.PERSONAL, True)
+    activity.add_slot(Meeting(Day.MONDAY, _format_str("12:00"), _format_str("14:30")))
+    activities.append(activity)
+    activities_option_1.append(activity)
+    activities_option_2.append(activity)
+
+    schedules = csp.extract_schedules(activities)
+    assert len(schedules) == 2
+    assert any(schedule.contains(activities_option_1) for schedule in schedules)
+    assert any(schedule.contains(activities_option_2) for schedule in schedules)
+
+
+def test_one_from_two_options():
+    activities_option_1 = []
+
+    activities = []
+    academic_activity = AcademicActivity("a", Type.LECTURE, True, "a", 1, 1, "a")
+    academic_activity.add_slot(Meeting(Day.SUNDAY, _format_str("10:00"), _format_str("11:00")))
+    activities.append(academic_activity)
+    activities_option_1.append(academic_activity)
+
+    academic_activity = AcademicActivity("a", Type.PRACTICE, True, "a", 2, 2, "a")
+    academic_activity.add_slot(Meeting(Day.SUNDAY, _format_str("12:00"), _format_str("14:30")))
+    activities.append(academic_activity)
+    activities_option_1.append(academic_activity)
+
+    academic_activity = AcademicActivity("a", Type.PRACTICE, True, "a", 2, 2, "a")
+    academic_activity.add_slot(Meeting(Day.MONDAY, _format_str("13:00"), _format_str("13:30")))
+    activities.append(academic_activity)
+
+    activity = Activity("c", Type.PERSONAL, True)
+    activity.add_slot(Meeting(Day.MONDAY, _format_str("12:00"), _format_str("14:30")))
+    activities.append(activity)
+    activities_option_1.append(activity)
+
+    schedules = csp.extract_schedules(activities)
+    assert len(schedules) == 1
+    assert any(schedule.contains(activities_option_1) for schedule in schedules)
+
+
+def test_no_option():
+    activities = []
+    academic_activity = AcademicActivity("a", Type.LECTURE, True, "a", 1, 1, "a")
+    academic_activity.add_slot(Meeting(Day.SUNDAY, _format_str("10:00"), _format_str("11:00")))
+    activities.append(academic_activity)
+
+    academic_activity = AcademicActivity("a", Type.LAB, True, "a", 2, 2, "a")
+    academic_activity.add_slot(Meeting(Day.MONDAY, _format_str("12:00"), _format_str("13:30")))
+    activities.append(academic_activity)
+
+    activity = Activity("c", Type.PERSONAL, True)
+    activity.add_slot(Meeting(Day.MONDAY, _format_str("12:00"), _format_str("14:30")))
+    activities.append(activity)
+
+    schedules = csp.extract_schedules(activities)
+    assert len(schedules) == 0
