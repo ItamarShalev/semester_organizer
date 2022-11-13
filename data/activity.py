deb91a30b21a662aba9a5d837bd2f1e9d99f3cfb
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 21:41:49 +0200

    [BUGFIX] data: activity: Miscorrect is_crash_with_meeting call.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/activity.py
+++ data/activity.py
@@ -11,12 +11,12 @@ class Activity:
         self.days = {day: [] for day in Day}
 
     def add_slot(self, meeting):
-        if meeting.is_crash_with_meeting(self.days[meeting.day]):
+        if meeting.is_crash_with_meetings(self.days[meeting.day]):
             raise Exception("Meeting is crash with other meeting")
         self.days[meeting.day].append(meeting)
 
     def is_free_slot(self, meeting):
-        return not meeting.is_crash_with_meeting(self.days[meeting.day])
+        return not meeting.is_crash_with_meetings(self.days[meeting.day])
 
     def add_slots(self, meetings):
         for meeting in meetings:
