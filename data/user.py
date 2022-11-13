781f254d2b7b00a3293194df8dcfc21b6106b53f
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 23:35:30 +0200

    data: user: Implement __eq__ method.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/user.py
+++ data/user.py
@@ -4,3 +4,6 @@ class User:
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
+
+    def __eq__(self, other):
+        return self.username == other.username and self.password == other.password
