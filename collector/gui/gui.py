f35b61311be12dd478be8a0d6fb0c84a493239e9
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 05:07:42 +0200

    api: collector: gui: Add open_notification_windows and open_choose_format_window functions.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- collector/gui/gui.py
+++ collector/gui/gui.py
@@ -4,6 +4,7 @@ from data.academic_activity import AcademicActivity
 from data.activity import Activity
 from data.course import Course
 from data.user import User
+from data.output_format import OutputFormat
 
 
 def open_login_window() -> User:
@@ -17,4 +18,12 @@ def open_academic_activities_window(campuses: List[str], courses: List[Course]) 
 
 
 def open_custom_activities_windows() -> List[Activity]:
+    pass
+
+
+def open_notification_windows(message: str):
+    pass
+
+
+def open_choose_format_window() -> List[OutputFormat]:
     pass
