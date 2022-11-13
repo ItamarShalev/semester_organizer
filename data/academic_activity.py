1d8c685a5e3e6862d97ed264d60c07936a8f3024
Itamar Shalev <itamar1111111@gmail.com>
2022-11-13 04:51:50 +0200

    data: academic_activity: Add convert to course object function.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/academic_activity.py
+++ data/academic_activity.py
@@ -1,13 +1,17 @@
 from data.activity import Activity
+from data.course import Course
 from data.type import Type
 
 
 class AcademicActivity(Activity):
 
-    def __init__(self, name: str, activity_type: Type, is_must: bool,
-                 lecturer_name: str, course_number: int, parent_course_number: int, location: str):
+    def __init__(self, name: str, activity_type: Type, is_must: bool, lecturer_name: str, course_number: int,
+                 parent_course_number: int, location: str):
         super().__init__(name, activity_type, is_must)
         self.lecturer_name = lecturer_name
         self.course_number = course_number
         self.parent_course_number = parent_course_number
         self.location = location
+
+    def convert_to_course_object(self):
+        return Course(self.name, self.course_number, self.parent_course_number)
