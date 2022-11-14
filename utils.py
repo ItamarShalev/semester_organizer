0c157a50420cc10528383db3024174e959b249ba
Itamar Shalev <itamar1111111@gmail.com>
2022-11-15 00:01:03 +0200

    utils: Add logging.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import sys
 
 
 def get_root_path():
@@ -9,3 +11,16 @@ def get_database_path():
     if not os.path.exists(os.path.join(get_root_path(), "database")):
         os.makedirs(os.path.join(get_root_path(), "database"))
     return os.path.join(get_root_path(), "database")
+
+
+def config_logging_level(level=logging.DEBUG):
+    format_logging = "%(asctime)s %(name)s.%(funcName)s +%(lineno)s: %(message)s"
+    logging.basicConfig(stream=sys.stdout, datefmt="%H:%M:%S", level=level, format=format_logging)
+
+
+def get_logging():
+    return logging.getLogger(get_custom_software_name())
+
+
+def get_custom_software_name():
+    return "semester_organizer_lev"
