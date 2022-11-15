0c47ad96c3c40779c70e631cb778bbd242bf6e29
Itamar Shalev <itamar1111111@gmail.com>
2022-11-15 03:46:42 +0200

    project: Set logging level to info for main flow.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- __main__.py
+++ __main__.py
@@ -1,4 +1,8 @@
+import logging
+
+import utils
 from controller.controller import Controller
 
 if __name__ == '__main__':
+    utils.config_logging_level(logging.INFO)
     Controller().run_main_gui_flow()
