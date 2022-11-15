fa59452d74571694e2dfa46d8c6449efbb70414b
Itamar Shalev <itamar1111111@gmail.com>
2022-11-15 20:45:45 +0200

    tests: convertor: Add tests for convert_activities function.
    The tests will check the flow but not the real content inside.
    
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- convertor/convertor.py
+++ convertor/convertor.py
@@ -1,12 +1,12 @@
 from typing import List
 
 from data.output_format import OutputFormat
-from data.schedule import Schedule as schedule
+from data.schedule import Schedule
 
 
 class Convertor:
 
-    def convert_activities(self, schedules: List[schedule], folder_location: str, formats: List[OutputFormat]):
+    def convert_activities(self, schedules: List[Schedule], folder_location: str, formats: List[OutputFormat]):
         """
         The function will save each schedule in the folder location in the wanted formats.
         :param schedules: the schedules
---
+++ convertor/tests/test_convertor.py
@@ -0,0 +1,62 @@
+
+import os
+import shutil
+import pytest
+import utils
+
+from convertor.convertor import Convertor
+from data.output_format import OutputFormat
+from data.academic_activity import AcademicActivity
+from data.meeting import Meeting
+from data.type import Type
+from data.day import Day
+from data.schedule import Schedule
+
+
+def _create_scduale(file_name: str):
+    activity = AcademicActivity("a", Type.LECTURE, True, "a", 1, 100, "a")
+    activity.add_slot(Meeting(Day.MONDAY, "10:00", "12:00"))
+    scduale = Schedule("a", file_name, None, [activity])
+    return scduale
+
+
+@pytest.mark.skip(reason="Not implemented yet.")
+@pytest.mark.parametrize("file_type", list(OutputFormat))
+def test_convert_pdf(file_type: OutputFormat):
+    convertor = Convertor()
+    path = os.path.join(utils.get_results_path(), "test_results")
+    extension = file_type.value
+    schedules = []
+    for i in range(1, 10):
+        schedules.append(_create_scduale(f"option_{i}"))
+
+    convertor.convert_activities(schedules, path, [file_type])
+    for i in range(1, 10):
+        file_name = f"option_{i}.{extension}"
+        file_path = os.path.join(path, file_name)
+        assert os.path.isfile(f"{file_path}"), f"{file_name} is not exist"
+        assert os.path.getsize(f"{file_path}") > 0, f"{file_name} is empty"
+
+    shutil.rmtree(path)
+
+
+@pytest.mark.skip(reason="Not implemented yet.")
+def test_convert_all_types():
+    convertor = Convertor()
+    path = os.path.join(utils.get_results_path(), "test_results")
+    schedules = []
+    for i in range(1, 10):
+        schedules.append(_create_scduale(f"option_{i}"))
+
+    convertor.convert_activities(schedules, path, list(OutputFormat))
+
+    for file_type in OutputFormat:
+        extension = file_type.value
+        folder_type_path = os.path.join(path, file_type.name.lower())
+        for i in range(1, 10):
+            file_name = f"option_{i}.{extension}"
+            file_path = os.path.join(folder_type_path, file_name)
+            assert os.path.isfile(f"{file_path}"), f"{file_name} is not exist"
+            assert os.path.getsize(f"{file_path}") > 0, f"{file_name} is empty"
+
+    shutil.rmtree(path)
