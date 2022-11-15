0b227a503ca5c34069d515308219b451fc46d0a3
Itamar Shalev <itamar1111111@gmail.com>
2022-11-15 03:32:55 +0200

    data: Define attendance_required in courses and change the name in activities.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/activity.py
+++ data/activity.py
@@ -4,10 +4,10 @@ from data.day import Day
 
 class Activity:
 
-    def __init__(self, name: str, activity_type: Type, is_must: bool):
+    def __init__(self, name: str, activity_type: Type, attendance_required: bool):
         self.name = name
         self.type = activity_type
-        self.is_must = is_must
+        self.attendance_required = attendance_required
         self.days = {day: [] for day in Day}
 
     def add_slot(self, meeting):
@@ -28,7 +28,7 @@ class Activity:
         return any(self.is_crash_with_activity(activity) for activity in activities)
 
     def is_crash_with_activity(self, activity):
-        if not self.is_must or not activity.is_must:
+        if not self.attendance_required or not activity.attendance_required:
             return False
         for day in Day:
             all_meetings = [meeting for meetings in self.days.values() for meeting in meetings]
@@ -41,7 +41,8 @@ class Activity:
         return not any(meetings for meetings in self.days.values())
 
     def __eq__(self, other):
-        is_equals = self.name == other.name and self.type == other.type and self.is_must == other.is_must
+        is_equals = self.name == other.name and self.type == other.type
+        is_equals = is_equals and self.attendance_required == other.attendance_required
         for day, meetings in self.days.items():
             is_equals = is_equals and len(meetings) == len(other.days[day])
             for meeting in meetings:
@@ -49,7 +50,7 @@ class Activity:
         return is_equals
 
     def __str__(self):
-        return f"{self.name} {self.type} {self.is_must} {self.days}"
+        return f"{self.name} {self.type} {self.attendance_required} {self.days}"
 
     def __repr__(self):
         return str(self)
--- data/course.py
+++ data/course.py
@@ -1,3 +1,4 @@
+from data.type import Type
 
 
 class Course:
@@ -6,12 +7,16 @@ class Course:
         self.name = name
         self.course_number = course_number
         self.parent_course_number = parent_course_number
+        self.attendance_required = {type: True for type in [Type.LECTURE, Type.PRACTICE, Type.LAB]}
 
     def __eq__(self, other):
         is_equals = self.name == other.name
         is_equals = is_equals and self.course_number == other.course_number
         is_equals = is_equals and self.parent_course_number == other.parent_course_number
         return is_equals
+
+    def set_attendance_required(self, course_type: Type, required: bool):
+        self.attendance_required[course_type] = required
 
     def __str__(self):
         return f"{self.name} {self.course_number} {self.parent_course_number}"
