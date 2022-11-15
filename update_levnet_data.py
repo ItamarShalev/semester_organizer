d717d7193a0707f8494c09d7f38145f0f36d7592
Itamar Shalev <itamar1111111@gmail.com>
2022-11-15 20:51:26 +0200

    update_levnet_data: Set logging level to debug.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- update_levnet_data.py
+++ update_levnet_data.py
@@ -1,6 +1,8 @@
+import logging
+
 import utils
 from controller.controller import Controller
 
 if __name__ == '__main__':
-    utils.set_logging_to_file()
+    utils.set_logging_to_file(logging.DEBUG)
     Controller().run_update_levnet_data_flow()
