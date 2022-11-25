478bfb13e90389ad5839bdfc20a0a8b92dba1117
Itamar Shalev <itamar1111111@gmail.com>
2022-11-25 04:04:16 +0200

    tests: convertor: Refactor move to test class.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- convertor/tests/test_convertor.py
+++ convertor/tests/test_convertor.py
@@ -1,8 +1,8 @@
-
 import os
 import shutil
 import pytest
 import utils
+from conftest import TestClass
 
 from convertor.convertor import Convertor
 from data.output_format import OutputFormat
@@ -13,49 +13,48 @@ from data.day import Day
 from data.schedule import Schedule
 
 
-def _create_schedule(file_name: str):
-    activity = AcademicActivity("a", Type.LECTURE, True, "a", 1, 100, "a")
-    activity.add_slot(Meeting(Day.MONDAY, Meeting.str_to_time("10:00"), Meeting.str_to_time("12:00")))
-    return Schedule("a", file_name, "", [activity])
-
-
 @pytest.mark.skip(reason="Not implemented yet.")
-@pytest.mark.parametrize("file_type", list(OutputFormat))
-def test_convert_pdf(file_type: OutputFormat):
-    convertor = Convertor()
-    path = os.path.join(utils.get_results_path(), "test_results")
-    extension = file_type.value
-    schedules = []
-    for i in range(1, 10):
-        schedules.append(_create_schedule(f"option_{i}"))
+class TestConvertor(TestClass):
 
-    convertor.convert_activities(schedules, path, [file_type])
-    for i in range(1, 10):
-        file_name = f"option_{i}.{extension}"
-        file_path = os.path.join(path, file_name)
-        assert os.path.isfile(f"{file_path}"), f"{file_name} is not exist"
-        assert os.path.getsize(f"{file_path}") > 0, f"{file_name} is empty"
+    def _create_schedule(self, file_name: str):
+        activity = AcademicActivity("a", Type.LECTURE, True, "a", 1, 100, "a")
+        activity.add_slot(Meeting(Day.MONDAY, Meeting.str_to_time("10:00"), Meeting.str_to_time("12:00")))
+        return Schedule("a", file_name, "", [activity])
 
-    shutil.rmtree(path)
-
-
-@pytest.mark.skip(reason="Not implemented yet.")
-def test_convert_all_types():
-    convertor = Convertor()
-    path = os.path.join(utils.get_results_path(), "test_results")
-    schedules = []
-    for i in range(1, 10):
-        schedules.append(_create_schedule(f"option_{i}"))
-
-    convertor.convert_activities(schedules, path, list(OutputFormat))
-
-    for file_type in OutputFormat:
+    @pytest.mark.parametrize("file_type", list(OutputFormat))
+    def test_convert_pdf(self, file_type: OutputFormat):
+        convertor = Convertor()
+        path = os.path.join(utils.get_results_path(), "test_results")
         extension = file_type.value
-        folder_type_path = os.path.join(path, file_type.name.lower())
+        schedules = []
+        for i in range(1, 10):
+            schedules.append(self._create_schedule(f"option_{i}"))
+
+        convertor.convert_activities(schedules, path, [file_type])
         for i in range(1, 10):
             file_name = f"option_{i}.{extension}"
-            file_path = os.path.join(folder_type_path, file_name)
+            file_path = os.path.join(path, file_name)
             assert os.path.isfile(f"{file_path}"), f"{file_name} is not exist"
             assert os.path.getsize(f"{file_path}") > 0, f"{file_name} is empty"
 
-    shutil.rmtree(path)
+        shutil.rmtree(path)
+
+    def test_convert_all_types(self):
+        convertor = Convertor()
+        path = os.path.join(utils.get_results_path(), "test_results")
+        schedules = []
+        for i in range(1, 10):
+            schedules.append(self._create_schedule(f"option_{i}"))
+
+        convertor.convert_activities(schedules, path, list(OutputFormat))
+
+        for file_type in OutputFormat:
+            extension = file_type.value
+            folder_type_path = os.path.join(path, file_type.name.lower())
+            for i in range(1, 10):
+                file_name = f"option_{i}.{extension}"
+                file_path = os.path.join(folder_type_path, file_name)
+                assert os.path.isfile(f"{file_path}"), f"{file_name} is not exist"
+                assert os.path.getsize(f"{file_path}") > 0, f"{file_name} is empty"
+
+        shutil.rmtree(path)
