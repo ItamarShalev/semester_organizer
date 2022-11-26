c4550ad6862f5d280cd03ada316bae780efe602c
Itamar Shalev <itamar1111111@gmail.com>
2022-11-27 01:34:15 +0200

    collector: network: Add Network http object to collect the data by the http method.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- utils.py
+++ utils.py
@@ -22,6 +22,10 @@ def get_root_path():
     return os.path.dirname(os.path.abspath(__file__))
 
 
+def get_current_hebrew_year():
+    return 5783
+
+
 def get_database_path():
     if not os.path.exists(os.path.join(get_root_path(), "database")):
         os.makedirs(os.path.join(get_root_path(), "database"))
