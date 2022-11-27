81a8e637614a55d0a4bac9efbfec171028a819f2
Itamar Shalev <itamar1111111@gmail.com>
2022-11-27 05:17:19 +0200

    tests: controller: Open tests.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- controller/tests/test_controller.py
+++ controller/tests/test_controller.py
@@ -3,23 +3,19 @@ import utils
 
 from collector.db.db import Database
 from collector.network.network import NetworkHttp
-from convertor.convertor import Convertor
 from csp import csp
-from data.output_format import OutputFormat
 
 
 @pytest.mark.network()
-@pytest.mark.skip(reason="Not implemented yet.")
+@pytest.mark.network_http()
 class TestController:
 
     run_in_background = True
 
     def test_flow_without_gui_without_database(self):
 
-        database = Database()
-        convertor = Convertor()
         campus_name = utils.get_campus_name_test()
-        user = database.load_hard_coded_user_data()
+        user = Database().load_hard_coded_user_data()
         network = NetworkHttp(user)
         assert user, "Don't have user data to check."
 
@@ -30,29 +26,24 @@ class TestController:
 
         assert campus_name in campus_names, "Some campus names are missing."
 
-        database.save_campus_names(campus_names)
         courses = network.extract_all_courses(campus_name)
         assert courses, "Can't extract courses from the server."
 
         course = utils.get_course_data_test()
         assert course in courses, "Some courses are missing."
 
-        database.save_courses_data(courses)
         academic_activities, missing_courses_names = network.extract_academic_activities_data(campus_name, [course])
         missing_meetings_data = any(activity.no_meetings() for activity in academic_activities)
         assert not missing_courses_names, "The following courses don't have activities: " + ", ".join(
             missing_courses_names)
 
-        assert missing_meetings_data, "Can't extract academic activities from the server."
-        formats = list(OutputFormat)
+        assert not missing_meetings_data, "Can't extract academic activities from the server."
         schedules = csp.extract_schedules(academic_activities)
         assert schedules, "At least one schedule should be extracted."
-        convertor.convert_activities(schedules, "results", formats)
 
     def test_flow_without_gui_with_database(self):
 
         database = Database()
-        convertor = Convertor()
         campus_name = "מכון לב"
         user = database.load_hard_coded_user_data()
         network = NetworkHttp(user)
@@ -86,11 +77,9 @@ class TestController:
         missing_meetings_data = any(activity.no_meetings() for activity in academic_activities)
         assert not missing_meetings_data, "Can't extract academic activities from the server."
 
-        formats = list(OutputFormat)
         schedules = csp.extract_schedules(academic_activities)
         assert schedules, "At least one schedule should be extracted."
 
-        convertor.convert_activities(schedules, "results", formats)
         assert campus_names, "Don't have campus names to check."
 
         assert campus_name in campus_names, "Some campus names are missing."
