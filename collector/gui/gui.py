30a1211fd23bb374e465e676db82e9f8c4af6994
Itamar Shalev <itamar1111111@gmail.com>
2022-12-04 22:23:42 +0200

    [API] collector: gui: Add open_settings_window function.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- collector/gui/gui.py
+++ collector/gui/gui.py
@@ -1,9 +1,10 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 from data.activity import Activity
 from data.course import Course
 from data.output_format import OutputFormat
 from data.user import User
+from data.settings import Settings
 
 
 class Gui:
@@ -24,3 +25,16 @@ class Gui:
 
     def open_choose_format_window(self) -> List[OutputFormat]:
         pass
+
+    def open_settings_window(self, settings: Settings, campuses: List[str], years: Dict[str, int]) -> Settings:
+        """
+        This function will open the settings window.
+        it will show all the campus names that the user can choose from.
+        years will be dict that will contain the years in format [hebrew_year: value_of_year]
+        for example: {"תשפ"ג"
+        : 5783}
+        :param settings: the settings that will be shown in the window. the function will return the new settings.
+        :param campuses: all the campus names that the user can choose from.
+        :param years: all the years that the user can choose from.
+        :return: new object of Settings.
+        """
