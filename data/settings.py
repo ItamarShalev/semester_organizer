edb855adadf08f778083d6956777759338dc4b24
Itamar Shalev <itamar1111111@gmail.com>
2022-12-04 22:23:42 +0200

    [API] collector: gui: Add open_settings_window function.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

---
+++ data/settings.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from data.output_format import OutputFormat
+from data.semester import Semester
+import utils
+
+
+@dataclass
+class Settings:
+
+    attendance_required_all_courses: bool = True
+    campus_name: str = utils.get_campus_name_test()
+    year: int = utils.get_current_hebrew_year()
+    semester: Semester = utils.get_current_semester()
+    show_hertzog: bool = False
+    show_only_courses_with_free_places: bool = True
+    show_only_courses_active_classes: bool = True
+    output_formats: List[OutputFormat] = field(default_factory=lambda: list(OutputFormat))
