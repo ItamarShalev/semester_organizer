6c6af55be3764bb21fed67b7682e858e19dfb46b
Itamar Shalev <itamar1111111@gmail.com>
2022-12-05 21:27:41 +0200

    controller: main_flow: Implement run_main_gui_flow function.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/course.py
+++ data/course.py
@@ -11,7 +11,8 @@ class Course:
         self.name = name
         self.course_number = course_number
         self.parent_course_number = parent_course_number
-        self.attendance_required = {type: True for type in [Type.LECTURE, Type.PRACTICE, Type.LAB]}
+        self.attendance_required_for_lecture = True
+        self.attendance_required_for_practice = True
         self.activity_id = activity_id
         self.actual_course_number = actual_course_number
 
@@ -27,10 +28,24 @@ class Course:
         return hash((self.name, self.course_number, self.parent_course_number))
 
     def set_attendance_required(self, course_type: Type, required: bool):
-        self.attendance_required[course_type] = required
+        if course_type in [Type.LECTURE, Type.SEMINAR]:
+            self.attendance_required_for_lecture = required
+        elif course_type in [Type.LAB, Type.PRACTICE]:
+            self.attendance_required_for_practice = required
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return str(self)
+
+    def is_attendance_required(self, course_type: Type):
+        attendance_required = True
+
+        if course_type in [Type.LECTURE, Type.SEMINAR]:
+            attendance_required = self.attendance_required_for_lecture
+
+        elif course_type in [Type.LAB, Type.PRACTICE]:
+            attendance_required = self.attendance_required_for_practice
+
+        return attendance_required
