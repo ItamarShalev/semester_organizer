c6a6e94e4bffd3f4d06c72198338685412056b6a
Itamar Shalev <itamar1111111@gmail.com>
2022-12-05 21:23:00 +0200

    data: course_choice: Add default values for attendance_required fields.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/course_choice.py
+++ data/course_choice.py
@@ -6,6 +6,6 @@ from typing import List
 class CourseChoice:
     name: str
     available_teachers_for_lecture: List[str]
-    attendance_required_for_lecture: bool
     available_teachers_for_practice: List[str]
-    attendance_required_for_practice: bool
+    attendance_required_for_lecture: bool = True
+    attendance_required_for_practice: bool = True
