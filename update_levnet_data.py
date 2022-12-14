03338a886b63d69a352b39d730ad7b03ad2ba6e8
Itamar Shalev <itamar1111111@gmail.com>
2022-12-14 20:30:05 +0200

    utils: Remove set_logging_to_file function.
    No need to this function, use config_logging_level instead.
    
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- update_levnet_data.py
+++ update_levnet_data.py
@@ -5,5 +5,5 @@ from controller.controller import Controller
 
 if __name__ == '__main__':
     utils.init_project()
-    utils.set_logging_to_file(logging.DEBUG)
+    utils.config_logging_level(logging.DEBUG)
     Controller().run_update_levnet_data_flow()
