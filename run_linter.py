87d896dc2a1b54322aa555bbc01b97a5cfa37784
Itamar Shalev <itamar1111111@gmail.com>
2022-12-14 20:52:47 +0200

    run_linter: Add tool to help to debug run tests and linters.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

---
+++ run_linter.py
@@ -0,0 +1,108 @@
+import argparse
+import logging
+import os
+import shutil
+import subprocess
+import sys
+from contextlib import suppress
+
+import utils
+from collector.db.db import Database
+from data.user import User
+
+
+def get_all_python_files(test_files=False):
+    files_result = []
+    blocked_dirs = ["venv", ".idea", "results", "database", "logs", ".github", ".pytest_cache", "__pycache__", ".git"]
+    for root, dirs, files in os.walk(utils.ROOT_PATH):
+        dirs[:] = [d for d in dirs if d not in blocked_dirs]
+        for file in files:
+            if file.endswith(".py") and (not test_files or file.startswith("test_")):
+                files_result.append(os.path.join(root, file))
+    return files_result
+
+
+def pip_install(*arguments):
+    return_code = subprocess.call([sys.executable, "-m", "pip", "install", *arguments])
+    assert return_code == 0, "ERROR: pip failed to install, check your network connection"
+
+
+def clear_project():
+    folders_to_clear = [".pytest_cache", "__pycache__"]
+    files_to_clear = ["coverage.xml", ".coverage"]
+    for folder in folders_to_clear:
+        folder_path = os.path.join(utils.ROOT_PATH, folder)
+        shutil.rmtree(folder_path, ignore_errors=True)
+
+    for file in files_to_clear:
+        file_path = os.path.join(utils.ROOT_PATH, file)
+        with suppress(FileNotFoundError):
+            os.remove(file_path)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", help="The username user in the server", default=None)
+    parser.add_argument("-p", "--password", help="The password user in the server", default=None)
+    parser.add_argument("-i", "--install", help="Install all the needed packages", default=False, action="store_true")
+    parser.add_argument("-c", "--coverage", help="Run coverage", default=False, action="store_true")
+    parser.add_argument("-n", "--network", help="Run network_http pytest mark", default=False, action="store_true")
+    parser.add_argument("-a", "--all", help="Run all tests", default=False, action="store_true")
+    parser.add_argument("-v", "--verbose", help="Print more debug logs", default=False, action="store_true")
+    arguments = parser.parse_args()
+    return arguments
+
+
+def update_pip():
+    pip_install("--upgrade", "pip")
+
+
+def install_requirements():
+    pip_install("-r", "requirements.txt")
+
+
+def install_linters():
+    pip_install("pycodestyle")
+    pip_install("pylint")
+
+
+def get_user_data(argument_args):
+    if not args.username or not args.password:
+        user_data = Database().load_hard_coded_user_data()
+    else:
+        user_data = User(argument_args.username, argument_args.password)
+        Database().save_hard_coded_user_data(user_data)
+    return user_data
+
+
+def run_linter_and_tests(arguments):
+    pylint_cmd = "coverage run -m pytest".split(" ") if arguments.coverage else ["pytest"]
+    pytest_arguments = ['-m', 'not network']
+    if arguments.all:
+        pytest_arguments = ['--reruns', '2', '--reruns-delay', '5']
+    elif arguments.network:
+        pytest_arguments = ['-m', 'not network_driver', '--reruns', '2', '--reruns-delay', '5']
+    if arguments.verbose:
+        pytest_arguments += ['-v']
+
+    return_code = subprocess.call(["pycodestyle", *get_all_python_files()])
+    return_code += subprocess.call([*pylint_cmd, *get_all_python_files(True), *pytest_arguments])
+
+    if arguments.coverage:
+        network_path = os.path.join(utils.ROOT_PATH, "collector", "network", "network.py")
+        coverage_cmd = f"coverage report -m --omit={network_path} --fail-under=95".split(" ")
+        return_code += subprocess.call([*coverage_cmd])
+    assert return_code == 0, "ERROR: Linter failed, check the log file"
+
+
+if __name__ == '__main__':
+    utils.init_project()
+    clear_project()
+    args = get_args()
+    if args.install:
+        update_pip()
+        install_requirements()
+        install_linters()
+    utils.config_logging_level(logging.DEBUG if args.verbose else logging.INFO)
+    user = get_user_data(args)
+    run_linter_and_tests(args)
