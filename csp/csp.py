36eea841a7b994876c89dc69815749aed1cf5d2f
Itamar Shalev <itamar1111111@gmail.com>
2022-12-14 17:50:08 +0200

    csp: Add abillity to choose the favorite teachers.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- csp/csp.py
+++ csp/csp.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 
 from constraint import Problem
 
+from data.academic_activity import AcademicActivity
 from data.activity import Activity
 from data.course_choice import CourseChoice
 from data.schedule import Schedule
@@ -33,6 +34,25 @@ class CSP:
     def _is_consist_activity(self, activity_one, activity_two):
         return all(not activity.is_crash_with_activities(activity_one) for activity in activity_two)
 
+    def _is_consist_favorite_teachers(self, activities: List[Activity]):
+        """
+        Check if the activities consist the favorite teachers
+        :param activities: list of activities that contains only one activity
+        :param activities: List[Activity]
+        :return: bool
+        """
+        activity = activities[0]
+        if not self.courses_choices or not isinstance(activity, AcademicActivity):
+            return True
+        names_list = []
+        if activity.type in [Type.LECTURE, Type.SEMINAR]:
+            names_list = self.courses_choices.available_teachers_for_lecture
+        elif activity.type == [Type.PRACTICE, Type.LAB]:
+            names_list = self.courses_choices.available_teachers_for_practice
+        if not names_list:
+            return True
+        return activity.lecturer_name in names_list
+
     def extract_schedules(self, activities: List[Activity], courses_choices: Optional[List[CourseChoice]] = None,
                           settings: Settings = None) -> List[Schedule]:
         problem = Problem()
@@ -50,6 +70,7 @@ class CSP:
         all_names_activities = activities_by_name.keys()
 
         for name in all_names_activities:
+            problem.addConstraint(self._is_consist_favorite_teachers, (name,))
             for other_name in all_names_activities:
                 if name == other_name:
                     continue
@@ -63,4 +84,7 @@ class CSP:
             schedule_result.append(schedule)
             option_counter += 1
 
+        if not schedule_result and courses_choices:
+            # If there are no schedules, try to find schedules without favorite teachers
+            return self.extract_schedules(activities, None, settings)
         return schedule_result
