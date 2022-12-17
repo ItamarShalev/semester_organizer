82248e9f74a252c889af81e863488784c550da68
Itamar Shalev <itamar1111111@gmail.com>
2022-12-17 17:51:47 +0200

    tests: controller: Implement test to run_main_gui_flow.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- controller/tests/test_controller.py
+++ controller/tests/test_controller.py
@@ -1,16 +1,69 @@
-import pytest
-import utils
+from typing import List, Dict
 
+from unittest.mock import MagicMock
+import pytest
+
+import utils
 from collector.db.db import Database
+from collector.gui.gui import MessageType
 from collector.network.network import NetworkHttp
+from controller.controller import Controller
 from csp.csp import CSP
+from data.course_choice import CourseChoice
+from data.settings import Settings
 
 
 @pytest.mark.network()
 @pytest.mark.network_http()
 class TestController:
 
-    run_in_background = True
+    @staticmethod
+    def _open_academic_activities_window_mock(ask_attendance_required: bool,
+                                              course_choices: Dict[str, CourseChoice]) -> Dict[str, CourseChoice]:
+        message = f"open_academic_activities_window_mock was called. with {ask_attendance_required} and " \
+                  f"{course_choices.keys()}"
+        utils.get_logging().debug(message)
+        name = utils.get_course_data_test().name
+        return {name: CourseChoice(name, [], [])}
+
+    @staticmethod
+    def _open_notification_window_mock(message: str, message_type: MessageType = MessageType.INFO,
+                                       buttons: List[str] = None):
+        msg = f"open_notification_window_mock was called. with {message} and {message_type} and {buttons}"
+        utils.get_logging().debug(msg)
+
+    @staticmethod
+    def _open_settings_window_mock(settings: Settings, campuses: List[str], years: Dict[int, str]) -> Settings:
+        msg = f"open_settings_window was called. with {settings} and {campuses} and {years}"
+        utils.get_logging().debug(msg)
+        return settings
+
+    @staticmethod
+    def _get_gui_mock():
+        user = Database().load_hard_coded_user_data()
+        assert user, "Don't have user data to check."
+        gui_mock = MagicMock()
+        gui_mock.open_login_window = MagicMock(return_value=user)
+        gui_mock.open_academic_activities_window = MagicMock(
+            side_effect=TestController._open_academic_activities_window_mock)
+        gui_mock.open_personal_activities_window = MagicMock(return_value=[])
+        gui_mock.open_notification_window = MagicMock(side_effect=TestController._open_notification_window_mock)
+        gui_mock.open_settings_window = MagicMock(side_effect=TestController._open_settings_window_mock)
+        return gui_mock
+
+    def test_run_main_gui_flow(self):
+        gui_mock = TestController._get_gui_mock()
+        convertor_mock = MagicMock()
+        convertor_mock.convert_activities = MagicMock()
+        controller = Controller()
+        controller.gui = gui_mock
+        controller.convertor = convertor_mock
+        controller.run_main_gui_flow()
+        results_dir = utils.get_results_path()
+        assert results_dir, "Can't get results path."
+        result_success_message = f"The schedules were saved in the {results_dir} folder"
+        controller.gui.open_notification_window.assert_called_with(result_success_message)
+        controller.convertor.convert_activities.assert_called_once()
 
     def test_flow_without_gui_without_database(self):
         csp = CSP()
