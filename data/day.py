8815a81a8ccd71825cb399c54a9afcb408d38ee6
Itamar Shalev <itamar1111111@gmail.com>
2023-01-04 14:52:18 +0200

    data: day: Implement str function and make enum day comparable.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/day.py
+++ data/day.py
@@ -1,10 +1,16 @@
-from enum import Enum, auto
+from enum import IntEnum, auto
 
 
-class Day(Enum):
+class Day(IntEnum):
     SUNDAY = auto()
     MONDAY = auto()
     TUESDAY = auto()
     WEDNESDAY = auto()
     THURSDAY = auto()
     FRIDAY = auto()
+
+    def __str__(self):
+        return self.name.capitalize()
+
+    def __repr__(self):
+        return str(self)
