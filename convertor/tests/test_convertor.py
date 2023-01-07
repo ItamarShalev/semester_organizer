5c44687a8cf06eb251e06a0ed4625eac3657346b
Itamar Shalev <itamar1111111@gmail.com>
2023-01-08 01:45:03 +0200

    tests: convertor: Add personal activity to convertor tests.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- convertor/tests/test_convertor.py
+++ convertor/tests/test_convertor.py
@@ -6,6 +6,7 @@ import pytest
 import utils
 from convertor.convertor import Convertor
 from data.academic_activity import AcademicActivity
+from data.activity import Activity
 from data.day import Day
 from data.meeting import Meeting
 from data.output_format import OutputFormat
@@ -33,8 +34,12 @@ class TestConvertor:
         for i in range(1, 10):
             schedules.append(TestConvertor._create_schedule(f"option_{i}"))
 
+        activity = Activity("שם", Type.PERSONAL, True)
+        activity.add_slot(Meeting(Day.FRIDAY, "10:00", "12:00"))
+        schedules.append(Schedule("שם", f"option_{10}", "", [activity]))
+
         convertor.convert_activities(schedules, path, [file_type])
-        for i in range(1, 10):
+        for i in range(1, 11):
             file_name = f"option_{i}.{extension}"
             file_path = os.path.join(path, file_name)
             assert os.path.isfile(f"{file_path}"), f"{file_name} is not exist"
@@ -49,13 +54,16 @@ class TestConvertor:
 
         for i in range(1, 10):
             schedules.append(TestConvertor._create_schedule(f"option_{i}"))
+        activity = Activity("שם", Type.PERSONAL, True)
+        activity.add_slot(Meeting(Day.FRIDAY, "10:00", "12:00"))
+        schedules.append(Schedule("שם", f"option_{10}", "", [activity]))
 
         convertor.convert_activities(schedules, path, list(OutputFormat))
 
         for file_type in OutputFormat:
             extension = file_type.value
             folder_type_path = os.path.join(path, file_type.name.lower())
-            for i in range(1, 10):
+            for i in range(1, 11):
                 file_name = f"option_{i}.{extension}"
                 file_path = os.path.join(folder_type_path, file_name)
                 assert os.path.isfile(f"{file_path}"), f"{file_name} is not exist"
