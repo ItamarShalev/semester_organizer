145d7728ee35dbcbcef2d5fd9f5eb43848c71495
Itamar Shalev <itamar1111111@gmail.com>
2023-01-08 12:03:40 +0200

    tests: controller: Remove unuses tests.
    Since we are able to test the main flow, no need for those.
    
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- controller/tests/test_controller.py
+++ controller/tests/test_controller.py
@@ -5,16 +5,14 @@ from unittest.mock import MagicMock
 import pytest
 
 import utils
-from collector.db.db import Database
 from collector.gui.gui import MessageType
-from collector.network.network import NetworkHttp
 from controller.controller import Controller
 from convertor.convertor import Convertor
-from csp.csp import CSP
 from data.course_choice import CourseChoice
 from data.language import Language
 from data.settings import Settings
 from data import translation
+from data.user import User
 
 
 @pytest.mark.network()
@@ -54,10 +52,8 @@ class TestController:
 
     @staticmethod
     def _get_gui_mock():
-        user = Database().load_user_data()
-        assert user, "Don't have user data to check."
         gui_mock = MagicMock()
-        gui_mock.open_login_window = MagicMock(return_value=user)
+        gui_mock.open_login_window = MagicMock(return_value=User("test", "test"))
         gui_mock.open_academic_activities_window = MagicMock(
             side_effect=TestController._open_academic_activities_window_mock)
         gui_mock.open_personal_activities_window = MagicMock(return_value=[])
@@ -66,6 +62,7 @@ class TestController:
         return gui_mock
 
     def test_run_main_gui_flow(self):
+        translation.config_language_text(Language.HEBREW)
         gui_mock = TestController._get_gui_mock()
         controller = Controller()
         convertor = Convertor()
@@ -86,75 +83,3 @@ class TestController:
         assert os.path.exists(results)
         files_count, _dirs = TestController._get_count_files_and_directory(results)
         assert files_count == 1
-
-    def test_flow_without_gui_without_database(self):
-        csp = CSP()
-        campus_name = utils.get_campus_name_test()
-        user = Database().load_user_data()
-        network = NetworkHttp(user)
-        assert user, "Don't have user data to check."
-
-        assert network.check_connection(), "Can't connect to the server."
-
-        campus_names = network.extract_campus_names()
-        assert campus_names, "Can't extract campus names from the server."
-
-        assert campus_name in campus_names, "Some campus names are missing."
-
-        courses = network.extract_all_courses(campus_name)
-        assert courses, "Can't extract courses from the server."
-
-        course = utils.get_course_data_test()
-        assert course in courses, "Some courses are missing."
-
-        academic_activities, missing_courses_names = network.extract_academic_activities_data(campus_name, [course])
-        missing_meetings_data = any(activity.no_meetings() for activity in academic_activities)
-        assert not missing_courses_names, "The following courses don't have activities: " + ", ".join(
-            missing_courses_names)
-
-        assert not missing_meetings_data, "Can't extract academic activities from the server."
-        schedules = csp.extract_schedules(academic_activities)
-        assert schedules, "At least one schedule should be extracted."
-
-    def test_flow_without_gui_with_database(self):
-        csp = CSP()
-        database = Database()
-        campus_name = "מכון לב"
-        user = database.load_user_data()
-        network = NetworkHttp(user)
-        assert user, "Don't have user data to check."
-
-        assert network.check_connection(), "Can't connect to the server."
-
-        campus_names = database.load_campus_names()
-        if not campus_names:
-            campus_names = network.extract_campus_names()
-            assert campus_names, "Can't extract campus names from the server."
-            assert campus_name in campus_names, "Some campus names are missing."
-            database.save_campus_names(campus_names)
-
-        courses = database.load_courses_data()
-        course = utils.get_course_data_test()
-        campus_name = utils.get_campus_name_test()
-        if not courses:
-            courses = network.extract_all_courses(campus_name)
-
-            assert courses, "Can't extract courses from the server."
-            assert course in courses, "Some courses are missing."
-            database.save_courses_data(courses)
-
-        academic_activities = database.load_academic_activities_data(campus_name, [course])
-
-        if not academic_activities:
-            academic_activities, missings = network.extract_academic_activities_data(campus_name, [course])
-            assert not missings, "The following courses don't have activities: " + ", ".join(missings)
-
-        missing_meetings_data = any(activity.no_meetings() for activity in academic_activities)
-        assert not missing_meetings_data, "Can't extract academic activities from the server."
-
-        schedules = csp.extract_schedules(academic_activities)
-        assert schedules, "At least one schedule should be extracted."
-
-        assert campus_names, "Don't have campus names to check."
-
-        assert campus_name in campus_names, "Some campus names are missing."
