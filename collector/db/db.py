40f33db1f0421094215e104a056349d60de0b38c
Itamar Shalev <itamar1111111@gmail.com>
2023-01-08 06:30:05 +0200

    collector: db: Remove deprected functions that will be replaced.
    Prepare to offline method.
    
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- collector/db/db.py
+++ collector/db/db.py
@@ -328,11 +328,7 @@ class Database:
             return campus_names
 
     def save_courses_data(self, courses: List[Course]):
-        courses_set = set(courses)
-        courses_set.update(set(self.load_courses_data()))
-        with open(Database.COURSES_DATA_FILE_PATH, "w", encoding=utils.ENCODING) as file:
-            for course in courses_set:
-                file.write(f"{course.name};{course.course_number};{course.parent_course_number}\n")
+        raise AttributeError("This function is deprecated.")
 
     def load_current_versions(self) -> Tuple[Optional[str], Optional[str]]:
         if not os.path.isfile(Database.VERSIONS_PATH):
@@ -355,129 +351,28 @@ class Database:
         self.save_settings(settings)
 
     def save_academic_activities_data(self, campus_name: str, academic_activities: List[AcademicActivity]):
-        activities_names = {activity.name: activity for activity in academic_activities}
-        was_exists = os.path.exists(Database.ACTIVITIES_DATA_DATABASE_PATH)
-        with database.connect(Database.ACTIVITIES_DATA_DATABASE_PATH) as connection:
-            cursor = connection.cursor()
-            # Create the table if not exists.
-            if not was_exists:
-                cursor.execute("CREATE TABLE activities (activity_id TEXT PRIMARY KEY, campus_name TEXT, "
-                               "activity_name TEXT, type INTEGER ,course_number INTEGER, lecturer TEXT, "
-                               "parent_course_number INTEGER, location TEXT);")
-                cursor.execute("CREATE TABLE meetings (day INTEGER, start_time TEXT, end_time TEXT, activity_id TEXT, "
-                               "FOREIGN KEY(activity_id) REFERENCES activities(activity_id) ON DELETE CASCADE "
-                               "UNIQUE(day, start_time, end_time, activity_id) ON CONFLICT IGNORE);")
-
-            # Delete all the activities that inside with the same name, to avoid old data.
-            for activity_name in activities_names:
-                cursor.execute("DELETE FROM activities WHERE campus_name = (?) AND activity_name = (?);",
-                               (campus_name, activity_name))
-
-            # Insert the new data.
-            for activity in academic_activities:
-                assert activity.activity_id, "The activity id is empty."
-                cursor.execute("INSERT INTO activities VALUES (?, ?, ?, ?, ?, ?, ?, ?);", (
-                    activity.activity_id, campus_name, activity.name, activity.type.value, activity.course_number,
-                    activity.lecturer_name, activity.parent_course_number, activity.location))
-                for meeting in activity.meetings:
-                    cursor.execute("INSERT INTO meetings VALUES (?, ?, ?, ?);", (
-                        meeting.day.value, meeting.get_string_start_time(), meeting.get_string_end_time(),
-                        activity.activity_id))
-            connection.commit()
-            cursor.close()
+        raise AttributeError("This function is deprecated.")
 
     def save_campus_names(self, names: List[str]):
-        with open(Database.CAMPUS_NAMES_FILE_PATH, "w", encoding=utils.ENCODING) as file:
-            file.write("\n".join(names))
+        raise AttributeError("This function is deprecated.")
 
     def load_courses_data(self) -> List[Course]:
-        if not os.path.isfile(Database.COURSES_DATA_FILE_PATH):
-            return []
-        with open(Database.COURSES_DATA_FILE_PATH, "r", encoding=utils.ENCODING) as file:
-            courses = []
-            for line in file:
-                line = line.strip()
-                course_name, course_number, parent_course_number = line.split(";")
-                course = Course(course_name, int(course_number), int(parent_course_number))
-                courses.append(course)
-            return courses
+        raise AttributeError("This function is deprecated.")
 
     def clear_courses_data(self):
-        if os.path.exists(Database.COURSES_DATA_FILE_PATH):
-            os.remove(Database.COURSES_DATA_FILE_PATH)
+        raise AttributeError("This function is deprecated.")
 
     def clear_academic_activities_data(self, campus_name: Optional[str] = None):
-        """
-        The function clears the academic activities data.
-        :param campus_name: if None, the function will clear all the academic activities data.
-        :return:
-        """
-        # If the database not exists, there is nothing to clear.
-        if not os.path.exists(Database.ACTIVITIES_DATA_DATABASE_PATH):
-            return
-        if campus_name is None:
-            with database.connect(Database.ACTIVITIES_DATA_DATABASE_PATH) as connection:
-                cursor = connection.cursor()
-                cursor.execute("DELETE FROM meetings;")
-                cursor.execute("DELETE FROM activities;")
-                connection.commit()
-                cursor.close()
-        else:
-            with database.connect(Database.ACTIVITIES_DATA_DATABASE_PATH) as connection:
-                cursor = connection.cursor()
-                cursor.execute("DELETE FROM activities WHERE campus_name = (?);", (campus_name,))
-                connection.commit()
-                cursor.close()
+        raise AttributeError("This function is deprecated.")
 
     def clear_campus_names(self):
-        if os.path.exists(Database.CAMPUS_NAMES_FILE_PATH):
-            os.remove(Database.CAMPUS_NAMES_FILE_PATH)
+        raise AttributeError("This function is deprecated.")
 
     def check_if_courses_data_exists(self, campus_name: str, courses: List[Course]) -> bool:
-        return bool(self.load_academic_activities_data(campus_name, courses))
+        raise AttributeError("This function is deprecated.")
 
     def load_academic_activities_data(self, campus_name: str, courses: List[Course]) -> List[AcademicActivity]:
-        """
-        The function loads the academic activities data from the database.
-        if the database does not exist, the function will return an empty list.
-        if courses is empty, the function will return all the academic activities data by campus name.
-        else the function will return the academic activities data by the campus name and the courses' names.
-        """
-
-        # If the database does not exist, return an empty list.
-        if not os.path.exists(Database.ACTIVITIES_DATA_DATABASE_PATH):
-            return []
-        activities = []
-        with database.connect(Database.ACTIVITIES_DATA_DATABASE_PATH) as connection:
-            cursor = connection.cursor()
-            # If courses is empty, return all the academic activities data by campus name.
-            if not courses:
-                cursor.execute("SELECT * FROM activities WHERE campus_name = (?);", (campus_name,))
-            else:
-                # Else, return the academic activities data by the campus name and the courses' names.
-                courses_names = [course.name for course in courses]
-                sql = f"SELECT * FROM activities WHERE campus_name = (?) AND activity_name IN " \
-                      f"({','.join(['?'] * len(courses_names))});"
-                cursor.execute(sql, (campus_name, *courses_names))
-
-            # Parse the data.
-            activities_data = cursor.fetchall()
-
-            for activity_data in activities_data:
-                activity_id, __, name, activity_type, course_number, lecturer, p_course_number, location = activity_data
-                activity = AcademicActivity(name, Type(activity_type), True, lecturer, course_number, p_course_number,
-                                            location, activity_id)
-                cursor.execute("SELECT * FROM meetings WHERE activity_id = (?);", (activity_id,))
-                meetings_data = cursor.fetchall()
-                # Parse the meetings data.
-                for meeting_data in meetings_data:
-                    day, start_time, end_time, activity_id = meeting_data
-                    if activity_id == activity.activity_id:
-                        meeting = Meeting(Day(day), Meeting.str_to_time(start_time), Meeting.str_to_time(end_time))
-                        activity.add_slot(meeting)
-                activities.append(activity)
-            cursor.close()
-        return activities
+        raise AttributeError("This function is deprecated.")
 
     def load_hard_coded_user_data(self) -> Optional[User]:
         """
@@ -496,9 +391,6 @@ class Database:
             return User(file.readline().strip(), file.readline().strip())
 
     def clear_all_data(self):
-        self.clear_courses_data()
-        self.clear_academic_activities_data()
-        self.clear_campus_names()
         self.clear_settings()
         self.clear_years()
         self.clear_database()
@@ -525,14 +417,7 @@ class Database:
             os.remove(Database.SETTINGS_FILE_PATH)
 
     def clear_data_old_than(self, days: int = DEFAULT_DAYS_TO_CLEAR):
-        if self._get_last_modified_by_days(Database.CAMPUS_NAMES_FILE_PATH) >= days:
-            self.clear_campus_names()
-        if self._get_last_modified_by_days(Database.COURSES_DATA_FILE_PATH) >= days:
-            self.clear_courses_data()
-        if self._get_last_modified_by_days(Database.ACTIVITIES_DATA_DATABASE_PATH) >= days:
-            self.clear_academic_activities_data()
-        if self._get_last_modified_by_days(Database.YEARS_FILE_PATH) >= days:
-            self.clear_years()
+        raise AttributeError("This function is deprecated.")
 
     def get_common_campuses_names(self) -> List[str]:
         campus_names = []
--- collector/db/tests/test_db.py
+++ collector/db/tests/test_db.py
@@ -14,8 +14,6 @@ from data.semester import Semester
 from data.settings import Settings
 from data.type import Type
 from data.user import User
-from data.day import Day
-from data.meeting import Meeting
 from data.output_format import OutputFormat
 
 
@@ -114,66 +112,11 @@ class TestDatabase:
         excepted_courses_choices = [CourseChoice(f"Course {i}", ["meir"], []) for i in range(10)]
         assert set(loaded_courses_choices.values()) == set(excepted_courses_choices)
 
-    def test_all(self):
-        database = Database()
+    def test_all(self, database):
         database.clear_all_data()
         database.init_database_tables()
         assert not database.load_settings()
         assert not database.load_campus_names(Language.ENGLISH)
-        assert not database.load_courses_data()
-        assert not database.load_academic_activities_data(utils.get_campus_name_test(), [])
-
-    def test_courses_data(self):
-        database = Database()
-        courses = [Course(f"Course {i}", i, i + 1000) for i in range(10)]
-        database.clear_courses_data()
-        database.save_courses_data(courses)
-        loaded_courses = database.load_courses_data()
-        assert set(courses) == set(loaded_courses)
-
-        database.clear_courses_data()
-        assert not database.load_courses_data()
-
-    def test_academic_activities(self):
-        database = Database()
-        academic_activities = []
-        campus_name = utils.get_campus_name_test()
-        for i in range(10):
-            academic_activity = AcademicActivity(f"name{i}", Type.LECTURE, True, campus_name, i, i + 1000, "a", str(i))
-            academic_activities.append(academic_activity)
-        academic_activities[0].add_slot(Meeting(Day.MONDAY, "10:00", "12:00"))
-        courses = AcademicActivity.extract_courses_data(academic_activities)
-        database.clear_academic_activities_data()
-        database.save_academic_activities_data(campus_name, academic_activities)
-        assert database.check_if_courses_data_exists(campus_name, courses)
-
-        loaded_academic_activities = database.load_academic_activities_data(campus_name, courses)
-        assert set(academic_activities).issubset(set(loaded_academic_activities))
-
-        database.clear_campus_names()
-        assert not database.load_campus_names()
-
-        database.save_academic_activities_data(campus_name, academic_activities)
-        academic_activities[0].type = Type.LAB
-        academic_activities.append(
-            AcademicActivity(f"Course {30}", Type.LECTURE, True, f"A {30}", 30, 30 + 1000, "", activity_id=f"{30}"))
-        database.save_academic_activities_data(campus_name, academic_activities)
-        courses.clear()
-        courses.append(Course(f"Course {30}", 30, 30 + 1000))
-        courses.append(Course(f"Course {0}", 0, 0 + 1000))
-        assert database.check_if_courses_data_exists(campus_name, courses)
-
-        loaded_academic_activities = database.load_academic_activities_data(campus_name, courses)
-        assert set(loaded_academic_activities).issubset(set(academic_activities))
-
-        loaded_academic_activities = database.load_academic_activities_data(campus_name, [])
-        assert loaded_academic_activities
-
-        assert utils.get_campus_name_test() in database.get_common_campuses_names()
-
-        database.clear_academic_activities_data(campus_name)
-        database.clear_data_old_than(0)
-        assert not database.load_academic_activities_data(campus_name, courses)
 
     def test_load_hard_coded_user_data(self, database):
         user = User("username", "password")
@@ -186,8 +129,7 @@ class TestDatabase:
         os.remove(Database.USER_NAME_FILE_PATH)
         assert database.load_hard_coded_user_data() is None
 
-    def test_settings(self):
-        database = Database()
+    def test_settings(self, database):
         database.clear_settings()
         assert database.load_settings() is None
 
@@ -201,8 +143,7 @@ class TestDatabase:
         database.clear_settings()
         assert database.load_settings() is None
 
-    def test_years(self):
-        database = Database()
+    def test_years(self, database):
         database.clear_years()
         assert not database.load_years()
 
