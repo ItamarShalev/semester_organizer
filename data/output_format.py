dc5ddc287e55b96e8dc03e01290efba468cd89dd
Itamar Shalev <itamar1111111@gmail.com>
2023-01-10 00:41:12 +0200

    data: output_format: Implement __str__ to return the name in lower.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/output_format.py
+++ data/output_format.py
@@ -5,3 +5,9 @@ class OutputFormat(Enum):
     CSV = "csv"
     EXCEL = "xlsx"
     IMAGE = "png"
+
+    def __str__(self):
+        return self.name.lower()
+
+    def __repr__(self):
+        return str(self)
