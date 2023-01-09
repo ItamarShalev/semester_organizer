164a52795c67c5c3370fe895167e4564cac0d2ed
Itamar Shalev <itamar1111111@gmail.com>
2023-01-09 19:00:50 +0200

    main: Add flow enum to args options.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@
 import argparse
 import logging
 import pathlib
+
 import argcomplete
 
 import utils
@@ -12,35 +13,46 @@ from controller.controller import Controller
 from data import translation
 from data.language import Language
 from data.user import User
+from data.flow import Flow
+from data.translation import _
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-g", "--gui", help="Run the program with GUI.", action="store_true")
-    parser.add_argument("-c", "--console", help="Run the program with console.", default=None, action="store_true")
+    parser.add_argument("-f", "--flow", help="Run the program with flow, can be gui or console.",
+                        default=Flow.GUI, choices=list(Flow), type=Flow.from_str)
     parser.add_argument("-u", "--username", help="The username user in the server", default=None, type=str)
     parser.add_argument("-p", "--password", help="The password user in the server", default=None, type=str)
     parser.add_argument("-l", "--language", help="Set the language of the program", choices=list(Language),
                         type=Language.from_str, default=Language.get_default())
-    parser.add_argument("--update_database", default="", type=pathlib.Path,
+    parser.add_argument("--database_path", default="", type=pathlib.Path,
                         help="Path to database file (.db) Update database by given .db file, "
                              "that can be downaloaded from the server (currently the gitub)")
     argcomplete.autocomplete(parser)
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
     utils.init_project()
-    utils.config_logging_level(logging.INFO)
+    utils.config_logging_level(logging.ERROR)
     args = get_args()
     translation.config_language_text(args.language)
     database = Database()
 
     if args.username and args.password:
         database.save_user_data(User(args.username, args.password))
-    if args.update_database.is_file():
-        database.update_database(args.update_database)
-    elif args.console:
+
+    elif args.flow is Flow.CONSOLE:
         Controller().run_console_flow()
-    else:
+
+    elif args.flow is Flow.GUI:
         Controller().run_main_gui_flow()
+
+    elif args.flow is Flow.UPDATE_DATABASE:
+        message = _("Database path is not a file or doesn't exists, the path given is: ")
+        assert args.database_path.is_file(), message + str(args.database_path)
+        database.update_database(args.database_path)
+
+
+if __name__ == '__main__':
+    main()
