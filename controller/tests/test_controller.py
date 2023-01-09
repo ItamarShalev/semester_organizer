04391b869a08cdc5b4f5655bb1ebb5b1b81511bf
Itamar Shalev <itamar1111111@gmail.com>
2023-01-09 06:43:00 +0200

    tests: controller: Organize the test for both english and hebrew.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- controller/tests/test_controller.py
+++ controller/tests/test_controller.py
@@ -85,10 +85,16 @@ class TestController:
     def test_flow_console(self, controller, language: Language):
         translation.config_language_text(language)
         Database().save_language(language)
-        controller.run_console_flow(1, "2, 8, 11, 14, 15, 35", 1, 2, 3, 1, 1, 1)
+        courses_for_english = "2, 8, 11, 14, 15, 35"
+        courses_for_hebrew = "1"
+        courses = courses_for_hebrew if language is Language.HEBREW else courses_for_english
+        controller.run_console_flow(1, courses, 1, 2, 3, 1, 1, 1)
         results = utils.get_results_path()
         # Check that the results file was created.
         # And contains only one file.
         assert os.path.exists(results)
         files_count, _dirs = TestController._get_count_files_and_directory(results)
-        assert files_count == 1
+        if language is Language.HEBREW:
+            assert files_count >= 1
+        elif language is Language.ENGLISH:
+            assert files_count == 1
