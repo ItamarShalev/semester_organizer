ed0df74c80c63a240096b5b4beefe7f62bb91a8e
Itamar Shalev <itamar1111111@gmail.com>
2025-02-23 03:29:55 +0200

    data: course_constraint: Use LF EOL always.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- data/course_constraint.py
+++ data/course_constraint.py
@@ -93,7 +93,7 @@ class CourseConstraint:
             "_comment": self.comment,
             "courses": [course.to_json(include_blocked_by, include_blocks, include_parallel) for course in all_courses]
         }
-        with open(file_path, 'w', encoding='utf-8') as json_file:
+        with open(file_path, 'w', encoding='utf-8', newline='\n') as json_file:
             json.dump(json_data, json_file, ensure_ascii=False, sort_keys=False, indent=4)
 
     def extract_courses_data(self, file_path: Path) -> Dict[int, ConstraintCourseData]:
